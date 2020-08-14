709460bf12130edd46675406764fed74e4a43620
Mr. Lan <fdevops@163.com>
2020-08-14 20:57:38 +0800

    dev

--- src/views/profile/userInfo.vue
+++ src/views/profile/userInfo.vue
@@ -72,7 +72,7 @@ export default {
     },
     close() {
       this.$store.dispatch('tagsView/delView', this.$route)
-      this.$router.push({ path: '/index' })
+      this.$router.push({ path: '/dashboard' })
     }
   }
 }
