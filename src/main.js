f7bf98d3ca62f8d7efdd81ddb126651d6228f6c7
lanyulei <934744141@qq.com>
2022-11-02 00:17:22 +0800

    update: 剔除无效代码。

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ import { parseTime, resetForm, addDateRange, selectDictLabel } from '@/utils/cos
 
 import './icons' // icon
 import './permission' // permission control
-import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
